69d755f08cd11088cab2e463397d671f5741e6b9
Zaid Mohammed Zainudeen <zainudeenzaidmohammed7@gmail.com>
2024-08-04 20:38:31 +0000

    Setup tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        NeonGreen: "hsl(150, 100%, 66%)",
+        DarkMode: "#003049",
+        NavColor: "#002039",
+      },
+      backgroundImage: {
+        heroBg: "url('/src/images/circles.svg')",
+      },
+    },
   },
   plugins: [],
 };
