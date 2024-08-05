33d51e7c83374b116f2a928548f91c7da71aedc3
Zaid Mohammed Zainudeen <zainudeenzaidmohammed7@gmail.com>
2024-08-05 21:41:37 +0000

    Updated projects page

--- src/data/ProjectsInfo.js
+++ src/data/ProjectsInfo.js
@@ -1,6 +1,12 @@
 import CatsNCheese from "../assets/projects/cats-n-cheese.png";
 import FootballTeamDashboard from "../assets/projects/football-team-dashboard.png";
 import CountriesApi from "../assets/projects/countries-api.png";
+import ClipboardLandingPage from "../assets/projects/clipboard-landing-page.png";
+import AdviceGenerator from "../assets/projects/advice-generator.png";
+import HuddleLandingPage from "../assets/projects/huddle-landing-page.png";
+import InteractiveRatingComponent from "../assets/projects/interactive-rating-component.png";
+import NewsletterSignUpForm from "../assets/projects/newsletter-sign-up-form-with-success-messege.png";
+import RecipePage from "../assets/projects/recipe-page.jpg";
 
 const ProjectsInfo = [
   {
@@ -23,6 +29,50 @@ const ProjectsInfo = [
     stack: ["React", "CSS", "API"],
     liveUrl: "https://countries-api-snowy-pi.vercel.app",
     githubUrl: "https://github.com/iamzaidmohammed/countries-api",
+  },
+  {
+    name: "Advice Generator",
+    image: AdviceGenerator,
+    stack: ["React", "Tailwind", "API"],
+    liveUrl: "https://advice-generator-lake.vercel.app/",
+    githubUrl: "https://github.com/iamzaidmohammed/advice-generator",
+  },
+  {
+    name: "Interactive Rating Component",
+    image: InteractiveRatingComponent,
+    stack: ["HTML", "CSS", "JS"],
+    liveUrl: "https://interactive-rating-component-chi-one.vercel.app/",
+    githubUrl: "https://github.com/iamzaidmohammed/Interative-Rating-Component",
+  },
+  {
+    name: "Newsletter Sign Up Form with Success Message",
+    image: NewsletterSignUpForm,
+    stack: ["HTML", "CSS", "JS"],
+    liveUrl:
+      "https://newsletter-sign-up-form-with-success-message-henna.vercel.app/",
+    githubUrl:
+      "https://github.com/iamzaidmohammed/Newsletter-sign-up-form-with-success-message",
+  },
+  {
+    name: "Clipboard Landing Page",
+    image: ClipboardLandingPage,
+    stack: ["HTML", "CSS"],
+    liveUrl: "https://clipboard-landing-page-alpha-six.vercel.app/",
+    githubUrl: "https://github.com/iamzaidmohammed/clipboard-landing-page",
+  },
+  {
+    name: "Huddle Landing Page",
+    image: HuddleLandingPage,
+    stack: ["HTML", "CSS"],
+    liveUrl: "https://huddle-landing-page-psi-pink.vercel.app/",
+    githubUrl: "https://github.com/iamzaidmohammed/huddle-landing-page",
+  },
+  {
+    name: "Recipe Page",
+    image: RecipePage,
+    stack: ["HTML", "CSS"],
+    liveUrl: "https://recipe-page-mocha-one.vercel.app/",
+    githubUrl: "https://github.com/iamzaidmohammed/recipe-page",
   },
 ];
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,6 +2,7 @@ import { FaSearch, FaAngleDown, FaAngleUp } from "react-icons/fa";
 import ProjectCard from "../components/ProjectCard.jsx";
 import { useEffect, useState } from "react";
 import projectsInfo from "../data/ProjectsInfo";
+import { Helmet } from "react-helmet";
 
 const Projects = () => {
   const [toggleSort, setToggleSort] = useState(false);
@@ -46,126 +47,138 @@ const Projects = () => {
   };
 
   return (
-    <main className="bg-DarkMode text-white min-h-dvh">
-      <section className="max-w-7xl flex flex-col items-center md:px-5 lg:px-20 xl:mx-auto">
-        <section className="py-10 w-full border-b-2">
-          <div className="px-10">
-            <h2 className="w-fit mx-auto text-6xl border-b-2 border-b-NeonGreen mb-10">
-              All Projects
-            </h2>
+    <>
+      <Helmet>
+        <title>Projects | iamzaidmohammed</title>
+      </Helmet>
 
-            <div className="w-full flex flex-col items-center gap-2 mb-10 sm:flex-row sm:gap-2">
-              <div className="relative w-full basis-2/3">
-                <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2 text-NeonGreen">
-                  <FaSearch />
-                </span>
-                <input
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                  placeholder="Search"
-                  className="appearance-none w-full rounded-l sm:rounded-l-none border-2 block pl-8 pr-6 py-2 bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white border-NeonGreen focus:border-2 focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
-                />
-              </div>
+      <main className="bg-DarkMode text-white min-h-dvh">
+        <section className="max-w-7xl flex flex-col items-center md:px-5 lg:px-20 xl:mx-auto">
+          <section className="py-10 w-full border-b-2">
+            <div className="px-10">
+              <h2
+                className="w-fit mx-auto text-6xl border-b-2 border-b-NeonGreen mb-10"
+                data-aos="fade-down"
+              >
+                All Projects
+              </h2>
 
-              <div className="flex items-center justify-center  sm:justify-end gap-2 w-full basis-1/3">
-                <div className="relative">
-                  <button
-                    className="flex items-center justify-between bg-NeonGreen text-DarkMode text-center p-2 sm:w-[150px]"
-                    onClick={() => {
-                      setToggleSort(!toggleSort);
-                      setToggleFilter(false);
-                    }}
-                  >
-                    Sort By {toggleSort ? <FaAngleUp /> : <FaAngleDown />}
-                  </button>
-                  {toggleSort && (
-                    <div className="absolute right-0 mt-2 w-[150px] bg-white text-DarkMode rounded shadow-lg z-10">
-                      <button
-                        className="block w-full text-left px-4 py-2 hover:bg-NeonGreen"
-                        onClick={() => handleSort("Most Recent")}
-                      >
-                        Most Recent
-                      </button>
-                      <button
-                        className="block w-full text-left px-4 py-2 hover:bg-NeonGreen"
-                        onClick={() => handleSort("Name")}
-                      >
-                        Name
-                      </button>
-                    </div>
-                  )}
-                </div>
-
-                <div className="relative">
-                  <button
-                    className="flex items-center justify-between bg-NeonGreen text-DarkMode text-center p-2 sm:w-[150px] sm:rounded-r"
-                    onClick={() => {
-                      setToggleFilter(!toggleFilter);
-                      setToggleSort(false);
-                    }}
-                  >
-                    Filter By {toggleFilter ? <FaAngleUp /> : <FaAngleDown />}
-                  </button>
-                  {toggleFilter && (
-                    <div className="absolute right-0 mt-2 w-[150px] bg-white text-DarkMode rounded shadow-lg z-10">
-                      {[
-                        "HTML",
-                        "CSS",
-                        "Tailwind",
-                        "JS",
-                        "React",
-                        "API",
-                        "PHP",
-                        "MySQL",
-                      ].map((filter) => (
-                        <label
-                          key={filter}
-                          className="block w-full text-left px-4 py-2 cursor-pointer hover:bg-NeonGreen"
-                        >
-                          <input
-                            type="checkbox"
-                            checked={selectedFilters.includes(filter)}
-                            onChange={() => handleFilterChange(filter)}
-                            className="mr-2"
-                          />
-                          {filter}
-                        </label>
-                      ))}
-                    </div>
-                  )}
-                </div>
-              </div>
-            </div>
-
-            <div
-              className={`${
-                filteredProjects.length === 0
-                  ? "w-full"
-                  : "w-full flex flex-col items-center gap-y-5 md:grid md:grid-cols-2 md:gap-x-5"
-              }`}
-            >
-              {filteredProjects.length > 0 ? (
-                filteredProjects.map((project, index) => (
-                  <ProjectCard
-                    key={index}
-                    image={project.image}
-                    projectName={project.name}
-                    liveUrl={project.liveUrl}
-                    githubUrl={project.githubUrl}
-                    stack={project.stack}
-                    data-aos={index % 2 === 0 ? "fade-right" : "fade-left"}
+              <div
+                className="w-full flex flex-col items-center gap-2 mb-10 sm:flex-row sm:gap-2"
+                data-aos="fade-down"
+              >
+                <div className="relative w-full basis-2/3">
+                  <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2 text-NeonGreen">
+                    <FaSearch />
+                  </span>
+                  <input
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="Search"
+                    className="appearance-none w-full rounded-l sm:rounded-l-none border-2 block pl-8 pr-6 py-2 bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white border-NeonGreen focus:border-2 focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
                   />
-                ))
-              ) : (
-                <div className="text-center text-2xl w-full">
-                  No projects found
                 </div>
-              )}
+
+                <div className="flex items-center justify-center  sm:justify-end gap-2 w-full basis-1/3">
+                  <div className="relative">
+                    <button
+                      className="flex items-center justify-between bg-NeonGreen text-DarkMode text-center p-2 sm:w-[150px]"
+                      onClick={() => {
+                        setToggleSort(!toggleSort);
+                        setToggleFilter(false);
+                      }}
+                    >
+                      Sort By {toggleSort ? <FaAngleUp /> : <FaAngleDown />}
+                    </button>
+                    {toggleSort && (
+                      <div className="absolute right-0 mt-2 w-[150px] bg-white text-DarkMode rounded shadow-lg z-10">
+                        <button
+                          className="block w-full text-left px-4 py-2 hover:bg-NeonGreen"
+                          onClick={() => handleSort("Most Recent")}
+                        >
+                          Most Recent
+                        </button>
+                        <button
+                          className="block w-full text-left px-4 py-2 hover:bg-NeonGreen"
+                          onClick={() => handleSort("Name")}
+                        >
+                          Name
+                        </button>
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="relative">
+                    <button
+                      className="flex items-center justify-between bg-NeonGreen text-DarkMode text-center p-2 sm:w-[150px] sm:rounded-r"
+                      onClick={() => {
+                        setToggleFilter(!toggleFilter);
+                        setToggleSort(false);
+                      }}
+                    >
+                      Filter By {toggleFilter ? <FaAngleUp /> : <FaAngleDown />}
+                    </button>
+                    {toggleFilter && (
+                      <div className="absolute right-0 mt-2 w-[150px] bg-white text-DarkMode rounded shadow-lg z-10">
+                        {[
+                          "HTML",
+                          "CSS",
+                          "Tailwind",
+                          "JS",
+                          "React",
+                          "API",
+                          "PHP",
+                          "MySQL",
+                        ].map((filter) => (
+                          <label
+                            key={filter}
+                            className="block w-full text-left px-4 py-2 cursor-pointer hover:bg-NeonGreen"
+                          >
+                            <input
+                              type="checkbox"
+                              checked={selectedFilters.includes(filter)}
+                              onChange={() => handleFilterChange(filter)}
+                              className="mr-2"
+                            />
+                            {filter}
+                          </label>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+
+              <div
+                className={`${
+                  filteredProjects.length === 0
+                    ? "w-full"
+                    : "w-full flex flex-col items-center gap-y-5 md:grid md:grid-cols-2 md:gap-x-5"
+                }`}
+              >
+                {filteredProjects.length > 0 ? (
+                  filteredProjects.map((project, index) => (
+                    <ProjectCard
+                      key={index}
+                      image={project.image}
+                      projectName={project.name}
+                      liveUrl={project.liveUrl}
+                      githubUrl={project.githubUrl}
+                      stack={project.stack}
+                      // data-aos={index % 2 === 0 ? "fade-right" : "fade-left"}
+                    />
+                  ))
+                ) : (
+                  <div className="text-center text-2xl w-full">
+                    No projects found
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
+          </section>
         </section>
-      </section>
-    </main>
+      </main>
+    </>
   );
 };
 
